bbe56c8c2f26696dd26afc922a4c7b60ea4ad833
NewCastile <carlosaguanipa1@gmail.com>
2024-03-06 12:25:28 -0400

    update for dynamic data usage

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,34 +1,38 @@
-import { NextResponse } from "next/server";
+"use server";
+
+import { NextRequest, NextResponse } from "next/server";
 
 import { INITIAL_PAGE_ROUTE } from "@/constants";
 import { createClient } from "@/utils/supabase/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/server-side/nextjs
+  const { searchParams, origin } = request.nextUrl;
+  const code = searchParams.get("code");
+
   try {
-    const requestUrl = new URL(request.url);
-
-    const code = requestUrl.searchParams.get("code");
-    const origin = requestUrl.origin;
-
     if (code) {
       const supabase = createClient();
       const { error } = await supabase.auth.exchangeCodeForSession(code);
 
       if (error) {
         return NextResponse.redirect(`${origin}/login`);
+      } else {
+        return NextResponse.redirect(`${origin}/${INITIAL_PAGE_ROUTE}`);
       }
     } else {
       return NextResponse.redirect(`${origin}/login`);
     }
-
-    // URL to redirect to after sign up process completes
-    return NextResponse.redirect(`${origin}/${INITIAL_PAGE_ROUTE}`);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
+
+    const loginPageErrorParams = `message=${encodeURI(
+      "Something went wrong while login you in",
+    )}&status=${400}&name=${encodeURI("Unkwown OAuth Error")}`;
+
     if (error instanceof Error && "status" in error) {
       const requestUrl = new URL(request.url);
 
@@ -38,13 +42,11 @@ export async function GET(request: Request) {
         return NextResponse.redirect(
           `${origin}/login?message=${error.message}&status=${error.status}&name=${error.name}`,
         );
+      } else {
+        return NextResponse.redirect(`${origin}/login?${loginPageErrorParams}`);
       }
-
-      return NextResponse.redirect(
-        `${origin}/login?message=${encodeURI(
-          "Something went wrong while login you in",
-        )}&status=${400}&name=${encodeURI("Unkwown OAuth Error")}`,
-      );
+    } else {
+      return NextResponse.redirect(`${origin}/login?${loginPageErrorParams}`);
     }
   }
 }

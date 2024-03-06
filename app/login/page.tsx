c2fe0ad546a6af48f7dbc64b410482d7f6b18459
NewCastile <carlosaguanipa1@gmail.com>
2024-03-06 17:26:12 -0400

    update for auth-state redirection

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,8 @@
+import { redirect } from "next/navigation";
+
+import { INITIAL_PAGE_ROUTE } from "@/constants";
+import { createClient } from "@/utils/supabase/server";
+
 import LoginView from "../components/views/login";
 
 const Login = async ({
@@ -7,11 +12,21 @@ const Login = async ({
 }) => {
   const { message, status } = searchParams;
 
-  return (
-    <div className={"flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md"}>
-      <LoginView {...{ message, status }} />
-    </div>
-  );
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return (
+      <div className={"flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md"}>
+        <LoginView {...{ message, status }} />
+      </div>
+    );
+  } else {
+    redirect(`${INITIAL_PAGE_ROUTE}`);
+  }
 };
 
 export default Login;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,20 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
 import NotFoundView from "./components/views/not-found";
 
-const NotFound = () => {
+const NotFound = async () => {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return <NotFoundView />;
 };
 

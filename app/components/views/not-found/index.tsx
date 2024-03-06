e72492dcde1fed62958917b93476f561ab200edc
NewCastile <carlosaguanipa1@gmail.com>
2024-03-06 17:23:40 -0400

    update for client side redirection

--- app/components/views/not-found/index.tsx
+++ app/components/views/not-found/index.tsx
@@ -1,12 +1,22 @@
-import NextLink from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const NotFoundView = () => {
+  const router = useRouter();
+
   return (
     <div className={"flex h-screen w-screen flex-col items-center justify-center text-gray-400"}>
       <p className={"text-5xl font-extrabold text-monokai-bg-contrast"}>Page Not Found</p>
-      <NextLink className={"text-3xl text-gray-400"} href={"/"}>
+      <button
+        className={"text-3xl text-gray-400"}
+        onClick={(e) => {
+          e.preventDefault();
+          router.back();
+        }}
+      >
         Return to the initial page
-      </NextLink>
+      </button>
     </div>
   );
 };

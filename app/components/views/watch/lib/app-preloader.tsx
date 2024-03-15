698661d44f6dacc7268b410983f3fc22fa4d8c8e
NewCastile <carlosaguanipa1@gmail.com>
2024-03-15 00:26:48 -0400

    update for props

--- app/components/views/watch/lib/app-preloader.tsx
+++ app/components/views/watch/lib/app-preloader.tsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import { BroadcastsState, setBroadcasts } from "@/lib/features/broadcasts/broadcasts-slice";
 import { loadFollowedChannels, loadFollowedStreams } from "@/lib/features/followed/followed-slice";
 import { setAccessToken } from "@/lib/features/tokens/access-token-slice";
-import { FollowedChannel, FollowedStream } from "@/types";
+import { FollowedEntity, FollowedStream } from "@/types";
 
 const AppPreloader = ({
   accessToken,
@@ -15,17 +15,17 @@ const AppPreloader = ({
   followedStreams,
 }: {
   accessToken: string;
-  refreshToken: string;
+  refreshToken?: string;
   screenBroadcasts: BroadcastsState["broadcasts"];
-  followedChannels: Array<FollowedStream | FollowedChannel>;
-  followedStreams: Array<FollowedStream>;
+  followedChannels?: Array<FollowedEntity>;
+  followedStreams?: Array<FollowedStream>;
 }) => {
   const dispatch = useDispatch();
 
+  if (followedChannels) dispatch(loadFollowedChannels(followedChannels));
+  if (followedStreams) dispatch(loadFollowedStreams(followedStreams));
   dispatch(setAccessToken({ accessToken, refreshToken }));
   dispatch(setBroadcasts(screenBroadcasts));
-  dispatch(loadFollowedChannels(followedChannels));
-  dispatch(loadFollowedStreams(followedStreams));
 
   return <></>;
 };

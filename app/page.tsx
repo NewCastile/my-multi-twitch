bbda6bae74ddc600d1d94b017a5daf670e7b6b78
NewCastile <carlosaguanipa1@gmail.com>
2024-03-16 16:25:00 -0400

    update for redirect to last visited page on authenticated user

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
+import { INITIAL_PAGE_ROUTE } from "@/constants";
 import { createClient } from "@/utils/supabase/server";
 
 import HomeView from "./components/views/home";
@@ -16,7 +17,18 @@ export default async function Index({
   } = await supabase.auth.getUser();
 
   if (user) {
-    redirect("/watch");
+    const { data: profiles } = await supabase.from("profiles").select("*").eq("id", user.id);
+
+    if (!profiles)
+      return <HomeView {...{ message: "No profile for this user was found ", status: "400" }} />;
+    const [profile] = profiles;
+
+    if (!profile)
+      return <HomeView {...{ message: "No profile for this user was found ", status: "400" }} />;
+
+    const { last_visited } = profile;
+
+    redirect(last_visited ? `/watch${last_visited}` : "/watch");
   }
 
   const { message, status } = searchParams;

394b6622197dbb99e66aca49d6bb3a95fd4cf5a3
NewCastile <carlosaguanipa1@gmail.com>
2024-03-20 19:08:02 -0400

    removed unnecessary metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,8 @@
-import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
 
 import NotFoundView from "./components/views/not-found";
-
-export const metadata: Metadata = {
-  title: "My Multi-Twitch - Error",
-};
 
 const NotFound = async () => {
   const supabase = createClient();

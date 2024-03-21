70c3535601a07ee18ce83f480cc2e4e5c4794e88
NewCastile <carlosaguanipa1@gmail.com>
2024-03-21 14:48:13 -0400

    removed unnecessary attributes

--- app/components/icons/logout-icon.tsx
+++ app/components/icons/logout-icon.tsx
@@ -7,9 +7,6 @@ const LogoutIcon = ({ size = DEFAULT_ICON_SIZE }: IconProps) => {
       fill={"none"}
       height={size}
       stroke={"currentColor"}
-      stroke-linecap={"round"}
-      stroke-linejoin={"round"}
-      stroke-width={"2"}
       viewBox={"0 0 24 24"}
       width={size}
       xmlns={"http://www.w3.org/2000/svg"}

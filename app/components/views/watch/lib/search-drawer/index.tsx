e469efbed3cac4d66f2576c71d008f7746a1c5e5
NewCastile <carlosaguanipa1@gmail.com>
2024-04-16 22:23:47 -0400

    update for CSR

--- app/components/views/watch/lib/search-drawer/index.tsx
+++ app/components/views/watch/lib/search-drawer/index.tsx
@@ -1,44 +1,65 @@
-"use server";
+"use client";
 
-import { fetchAppAccessToken } from "@/app/fetchers";
-import { createClient } from "@/utils/supabase/server";
+import { CloseIcon } from "@/app/components/icons/close-icon";
+import { SearchDrawerProvider } from "@/app/context/drawers";
+import useDrawer from "@/app/hooks/use-drawer";
+import { useDrawerRenderConditions } from "@/app/hooks/use-drawer-render-conditions";
 
-import AppPreloader from "../app-preloader";
+import SearchDrawerContent from "./search-drawer-content";
 
-import SearchDrawerClientComponent from "./search-drawer-cc";
+const SearchDrawer = () => {
+  const drawerId = "search-drawer";
 
-const SearchDrawer = async () => {
-  const supabase = createClient();
-
-  const userData = supabase.auth.getUser().then((res) => res.data.user);
-  const userAccessTokenData = supabase.auth
-    .getSession()
-    .then((res) => res.data.session?.access_token);
-
-  const [user, userAccessToken] = await Promise.all([userData, userAccessTokenData]);
-
-  if (!user || !userAccessToken) {
-    const fetchAppAccessTokenResponse = await fetchAppAccessToken();
-
-    if ("message" in fetchAppAccessTokenResponse) {
-      return <SearchDrawerClientComponent />;
-    }
-
-    const { access_token: appAccessToken } = fetchAppAccessTokenResponse;
-
-    return (
-      <>
-        <AppPreloader {...{ accessToken: appAccessToken }} />
-        <SearchDrawerClientComponent />
-      </>
-    );
-  }
+  const { drawer, drawerRef, isHidden } = useDrawer({ drawerId });
+  const { accessTokenAvailable, disabled } = useDrawerRenderConditions();
 
   return (
-    <>
-      <AppPreloader {...{ accessToken: userAccessToken }} />
-      <SearchDrawerClientComponent />
-    </>
+    <SearchDrawerProvider {...{ drawer, drawerId, drawerRef, isHidden }}>
+      {accessTokenAvailable && (
+        <button
+          aria-label={"open"}
+          className={
+            disabled
+              ? "btn-md cursor-not-allowed font-bold uppercase text-monokai-bg-contrast"
+              : "btn-md font-bold uppercase"
+          }
+          disabled={disabled}
+          type={"button"}
+          onClick={(e) => {
+            e.preventDefault();
+            if (drawer) {
+              drawer.show();
+            }
+          }}
+        >
+          search
+        </button>
+      )}
+
+      <div
+        ref={drawerRef}
+        className={
+          "fixed right-0 top-0 z-40 h-screen w-[25vw] translate-x-full overflow-y-auto bg-[#303025] p-4 pb-5 pt-20 text-monokai-white transition-transform"
+        }
+        tabIndex={-1}
+      >
+        <button
+          className={
+            "absolute end-2.5 top-2.5 inline-flex size-8 items-center justify-center rounded-lg bg-transparent text-sm text-monokai-white"
+          }
+          tabIndex={-1}
+          type={"button"}
+          onClick={() => {
+            if (drawer) {
+              drawer.hide();
+            }
+          }}
+        >
+          <CloseIcon />
+        </button>
+        <SearchDrawerContent />
+      </div>
+    </SearchDrawerProvider>
   );
 };
 

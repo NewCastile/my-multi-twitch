df640ef999be84d9c2174b4b7239512880cdd0f3
NewCastile <carlosaguanipa1@gmail.com>
2024-04-16 22:25:29 -0400

    update for code splitting

--- app/components/views/watch/lib/followed-drawer/followed-drawer-content/index.tsx
+++ app/components/views/watch/lib/followed-drawer/followed-drawer-content/index.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 import { CloseIcon } from "@/app/components/icons/close-icon";
 import useSearchFollowed from "@/app/hooks/use-search-followed";
 import useSelectBroadcasts from "@/app/hooks/use-select-broadcasts";
-import { FOLLOWED_ITEMS_PER_PAGE } from "@/constants";
-import { BroadcasterBasicInfo, FollowedEntity } from "@/types";
-import { isFollowedChannel } from "@/utils/type-guards";
 
 import SearchFilterButton from "../../search-drawer/search-drawer-content/lib/search-filter-button";
 import CancelSelectController from "../../shared/cancel-select-controller";
@@ -14,53 +11,23 @@ import ConfirmSelectController, {
   ConfirmSelectControllerIcon,
 } from "../../shared/confirm-select-controller";
 
-import FollowedChannelItem from "./lib/followed-channel-item";
+import Followeds from "./followeds";
+import { FollowedDrawerPagination } from "./lib/followed-drawer-pagination";
 import FollowedDrawerInput from "./lib/followed-input";
-import FollowedStreamItem from "./lib/followed-stream-item";
 
-const FollowedDrawerContent = ({ isError }: { isError?: boolean }) => {
+const FollowedDrawerContent = () => {
   const {
-    newUrl,
-    selectedBroadcasts,
-    selectAction,
-    isSelecting,
-    getIsOnScreen,
-    getIsSelected,
-    selectControlsHandlers: { closeSelect, onSelectHandler },
+    selectState: { newUrl, selectedBroadcasts, isSelecting, selectAction },
+    selectHandlers: { closeSelectHandler },
   } = useSelectBroadcasts({ selectAction: "add" });
 
   const [query, setQuery] = useState<string>("");
   const [liveOnly, setLiveOnly] = useState<boolean>(false);
 
-  const {
-    followeds,
-    filteredFolloweds,
-    pageToken,
-    shownFollowed,
-    controlButtonsHandler: { nextPageButtonOnClickHandler, prevPageButtonOnClickHandler },
-  } = useSearchFollowed({ query, liveOnly });
-
-  const getFollowedBasicInfo = (followed: FollowedEntity) => {
-    const { broadcaster_login, broadcaster_name }: BroadcasterBasicInfo = isFollowedChannel(
-      followed,
-    )
-      ? followed
-      : { broadcaster_login: followed.user_login, broadcaster_name: followed.user_name };
-
-    return { broadcaster_login, broadcaster_name };
-  };
-
-  const allFollowedsShown = useMemo(() => {
-    return pageToken > FOLLOWED_ITEMS_PER_PAGE;
-  }, [pageToken]);
-
-  const hasNoFollowedChannels = useMemo(() => {
-    return filteredFolloweds.length === 0;
-  }, [filteredFolloweds]);
-
-  const hasNextPage = useMemo(() => {
-    return shownFollowed.length < followeds.length;
-  }, [followeds, shownFollowed]);
+  const { shownFolloweds, paginationState, paginationHandlers } = useSearchFollowed({
+    query,
+    liveOnly,
+  });
 
   const searchInputOnChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
@@ -85,7 +52,7 @@ const FollowedDrawerContent = ({ isError }: { isError?: boolean }) => {
         <div className={"flex w-full flex-row items-center justify-end gap-2"}>
           {isSelecting && selectedBroadcasts.length > 0 && (
             <>
-              <CancelSelectController onClick={closeSelect}>
+              <CancelSelectController onClick={closeSelectHandler}>
                 <CloseIcon size={"0.6rem"} />
               </CancelSelectController>
               <ConfirmSelectController route={newUrl}>
@@ -94,69 +61,13 @@ const FollowedDrawerContent = ({ isError }: { isError?: boolean }) => {
             </>
           )}
         </div>
-        {isError ? (
-          <div className={"flex w-full flex-col items-center justify-center gap-4"}>
-            <p className={"w-full text-center"}>
-              Sorry could&apos;nt load your followed channels 😢
-            </p>
-            <p className={"w-full text-center"}>Try reloading the page.</p>
-          </div>
-        ) : hasNoFollowedChannels ? (
-          <p>You don&apos;t follow any channel 😶</p>
-        ) : (
-          <ul
-            className={
-              "flex size-full flex-col items-center justify-center divide-y-2 divide-monokai-bg-contrast overflow-y-auto overflow-x-hidden"
-            }
-          >
-            {filteredFolloweds.map((followed, followedIdx) => {
-              const { broadcaster_login, broadcaster_name } = getFollowedBasicInfo(followed);
-              const isSelected = getIsSelected({ broadcaster_login });
-              const isOnScreen = getIsOnScreen({ broadcaster_login });
-              const onClickHandler = () =>
-                onSelectHandler({
-                  broadcaster_login,
-                  broadcaster_name,
-                });
-
-              return isFollowedChannel(followed) ? (
-                <FollowedChannelItem
-                  key={followedIdx}
-                  {...{ followedChannel: followed, isSelected, isOnScreen, onClickHandler }}
-                />
-              ) : (
-                <FollowedStreamItem
-                  key={followedIdx}
-                  {...{ followedStream: followed, isSelected, isOnScreen, onClickHandler }}
-                />
-              );
-            })}
-          </ul>
-        )}
-        {!hasNoFollowedChannels && (
-          <div className={"flex w-full flex-row items-center justify-between"}>
-            {hasNextPage && (
-              <button
-                aria-label={"expand-followeds"}
-                className={"btn-sm bg-monokai-bg-secondary text-xs font-bold uppercase"}
-                tabIndex={-1}
-                onClick={nextPageButtonOnClickHandler}
-              >
-                Show more
-              </button>
-            )}
-            {allFollowedsShown && (
-              <button
-                aria-label={"collapse-followeds"}
-                className={"btn-sm bg-monokai-bg-secondary text-xs font-bold uppercase"}
-                tabIndex={-1}
-                onClick={prevPageButtonOnClickHandler}
-              >
-                Show less
-              </button>
-            )}
-          </div>
-        )}
+        <Followeds {...{ followeds: shownFolloweds }} />
+        <FollowedDrawerPagination
+          {...{
+            paginationState,
+            paginationHandlers,
+          }}
+        />
       </div>
     </div>
   );
--- app/components/views/watch/lib/search-drawer/search-drawer-content/index.tsx
+++ app/components/views/watch/lib/search-drawer/search-drawer-content/index.tsx
@@ -1,91 +1,35 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useFetchSearch } from "@/app/hooks/use-fetch-search";
 
-import { SpinnerIcon } from "@/app/components/icons/spinner-icon";
-import { SEARCH_ITEMS_PER_PAGE } from "@/constants";
-import { useLazySearchBroadcastQuery } from "@/lib/features/search/search-api-slice";
-import {
-  goToNextPage,
-  goToPrevPage,
-  loadNextPage,
-  loadSearch,
-  resetChannelSearch,
-} from "@/lib/features/search/search-channels-slice";
-import { useAppDispatch, useAppSelector } from "@/lib/store";
-import { ApiErrorResponse, SearchChannelResponse } from "@/types";
-import { isSearchChannelsResponse } from "@/utils/type-guards";
-
-import SearchBackButton from "./lib/search-back-button";
 import SearchChannelResultItem from "./lib/search-channel-result-item";
+import { SearchDrawerPagination } from "./lib/search-drawer-pagination";
 import SearchFilterButton from "./lib/search-filter-button";
 import SearchInput from "./lib/search-input";
-import SearchNextButton from "./lib/search-next-button";
 
 const SearchDrawerContent = () => {
-  const dispatch = useAppDispatch();
-  const { accessToken } = useAppSelector((state) => state.accessToken);
-  const { shownChannels, channels, pageToken } = useAppSelector((state) => state.searchChannels);
+  const {
+    shownChannels,
+    fetchState,
+    queryState,
+    paginationState,
+    searchFilterHandlers,
+    paginationHandlers,
+  } = useFetchSearch();
 
-  const [triggerNextSearch, { data, error, isLoading, isFetching, originalArgs }] =
-    useLazySearchBroadcastQuery();
+  const { data, isLoading, isError, isFetching } = fetchState;
 
-  const [value, setValue] = useState("");
-  const [liveOnly, setLiveOnly] = useState(false);
-  const onFirstPage = pageToken === SEARCH_ITEMS_PER_PAGE;
+  const { hasPagination, isOnFirstPage, pageToken } = paginationState;
+  const { query, liveOnly } = queryState;
 
-  useEffect(() => {
-    if (!data || !isSearchChannelsResponse(data)) return;
-    if (typeof originalArgs?.after === "string") {
-      dispatch(loadNextPage(data.data));
+  const {
+    nextButtonOnClickHandler,
+    prevButtonOnClickHandler,
+    searchInputOnChangeHandler,
+    searchInputOnKeyUpHandler,
+  } = paginationHandlers;
 
-      return;
-    }
-    dispatch(loadSearch(data.data));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch, data]);
-
-  useEffect(() => {
-    dispatch(resetChannelSearch());
-  }, [dispatch, liveOnly]);
-
-  const searchButtonOnClickHandler = () => {
-    if (!value) return;
-
-    triggerNextSearch({
-      broadcasterName: value,
-      liveOnly,
-      accessToken,
-    });
-  };
-
-  const hasPagination = (data?: SearchChannelResponse | ApiErrorResponse) => {
-    if (!data) return false;
-    if ("message" in data) return false;
-    if (!isSearchChannelsResponse(data)) return false;
-    if (!data.pagination.cursor) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  const nextButtonOnClickHandler = () => {
-    if (!value) return;
-    if (pageToken && shownChannels.length < channels.length) {
-      dispatch(goToNextPage());
-
-      return;
-    }
-    if (pageToken && data && isSearchChannelsResponse(data)) {
-      triggerNextSearch({
-        broadcasterName: value,
-        liveOnly,
-        accessToken,
-        after: data.pagination.cursor,
-      });
-    }
-  };
+  const { changeSearchFilter } = searchFilterHandlers;
 
   return (
     <div
@@ -95,29 +39,25 @@ const SearchDrawerContent = () => {
         <SearchFilterButton
           disabled={isFetching || isLoading}
           isActive={!liveOnly}
-          onClickHandler={() => setLiveOnly(false)}
+          onClickHandler={() => changeSearchFilter("ALL")}
         >
           All
         </SearchFilterButton>
         <SearchFilterButton
           disabled={isFetching || isLoading}
           isActive={liveOnly}
-          onClickHandler={() => setLiveOnly(true)}
+          onClickHandler={() => changeSearchFilter("LIVE_ONLY")}
         >
           Live only
         </SearchFilterButton>
       </div>
       <SearchInput
-        buttonOnClickHandler={searchButtonOnClickHandler}
-        value={value}
-        onChangeHandler={(e) => setValue(e.target.value)}
-        onKeyUpHandler={(e) => {
-          if (e.key === "Enter") {
-            searchButtonOnClickHandler();
-          }
-        }}
+        buttonOnClickHandler={() => nextButtonOnClickHandler()}
+        value={query}
+        onChangeHandler={(e) => searchInputOnChangeHandler(e)}
+        onKeyUpHandler={(e) => searchInputOnKeyUpHandler(e)}
       />
-      {error ? (
+      {isError ? (
         <p>An error has ocurred 😕. Please try again or reload the page</p>
       ) : !data ? null : "message" in data ? (
         <div className={"flex w-full flex-col items-center justify-center"}>
@@ -137,16 +77,13 @@ const SearchDrawerContent = () => {
           })}
         </ul>
       )}
-      <div className={"flex w-full flex-col items-center justify-center"}>
-        {isLoading || isFetching ? (
-          <SpinnerIcon />
-        ) : pageToken && hasPagination(data) ? (
-          <div className={"flex w-full flex-row items-center justify-between"}>
-            <SearchNextButton onClickHandler={nextButtonOnClickHandler} />
-            {!onFirstPage && <SearchBackButton onClickHandler={() => dispatch(goToPrevPage())} />}
-          </div>
-        ) : null}
-      </div>
+      <SearchDrawerPagination
+        {...{
+          searchState: { isFetching, isLoading },
+          paginationState: { hasPagination, isOnFirstPage, pageToken },
+          paginationHandlers: { nextButtonOnClickHandler, prevButtonOnClickHandler },
+        }}
+      />
     </div>
   );
 };
